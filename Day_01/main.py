6bac848583c22ec68774af18c47c8c36c22777ac
Galera115 <agalera13@gmail.com>
2023-12-02 19:03:38 +0100

    First iteration of Part 2. Still Wrong

--- Day_01/main.py
+++ Day_01/main.py
@@ -7,10 +7,22 @@ def read_input(file):
     return lines
 
 
+def get_numbers(line):
+    digits_letters = ['one', 'two', 'three', 'four', 'five',
+                      'six', 'seven', 'eight', 'nine']
+    all_numbers = findall(r'\d|' + r'|'.join(digits_letters), line)
+    for number in all_numbers:
+        if number in digits_letters:
+            all_numbers[all_numbers.index(number)] = digits_letters.index(number) + 1
+        else:
+            all_numbers[all_numbers.index(number)] = int(number)
+    return all_numbers
+
+
 if __name__ == '__main__':
     lines = read_input('Day_01/input.txt')
     sum = 0
     for line in lines:
-        numbers = [int(x) for x in line if x.isdigit()]
+        numbers = get_numbers(line)
         sum += numbers[0]*10 + numbers[-1]
     print(sum)
