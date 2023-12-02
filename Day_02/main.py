0319112c46603602cf62462a180dd191262fedab
Galera115 <agalera13@gmail.com>
2023-12-02 19:39:52 +0100

    Second Day. Second part finished

--- Day_02/main.py
+++ Day_02/main.py
@@ -8,6 +8,9 @@ def read_input(file):
 
 
 def split_line(line: str):
+    max_num_blues = 0
+    max_num_reds = 0
+    max_num_greens = 0
     game_id = int(line.split(':')[0].split(' ')[-1])
     game_sets = line.split(':')[1].split(';')
     game_sets = [game_set.split(',') for game_set in game_sets]
@@ -22,15 +25,19 @@ def split_line(line: str):
                 num_reds = int(re.findall(r'\d+', play)[0])
             elif 'green' in play:
                 num_greens = int(re.findall(r'\d+', play)[0])
-        if num_blues > 14 or num_greens > 13 or num_reds > 12:
-            return 0
-    return game_id
+        if num_blues > max_num_blues:
+            max_num_blues = num_blues
+        if num_reds > max_num_reds:
+            max_num_reds = num_reds
+        if num_greens > max_num_greens:
+            max_num_greens = num_greens
+    return max_num_blues * max_num_reds * max_num_greens
 
 
 if __name__ == '__main__':
     lines = read_input('Day_02/input.txt')
     sum = 0
     for line in lines:
-        game_id = split_line(line)
-        sum += game_id
+        power = split_line(line)
+        sum += power
     print(sum)
