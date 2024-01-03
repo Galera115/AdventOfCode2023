44dce538a6805c73e3be9d140109ce3fee442612
Galera115 <agalera13@gmail.com>
2024-01-03 12:47:46 +0100

    The version with the LCM for part 2 works way faster

--- Day_08/main.py
+++ Day_08/main.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import re
+from math import gcd
 
 @dataclass
 class Node:
@@ -23,22 +24,50 @@ def search_node(name, node_list):
         if node.name == name:
             return node
 
+
+def get_steps(node_list, path, starter_node):
+    steps = 0
+    found_path = False
+    while not found_path:
+        for step in path:
+            if step == 'L':
+                starter_node = search_node(starter_node.left, node_list)
+            else:
+                starter_node = search_node(starter_node.right, node_list)
+            steps += 1
+            if starter_node.name == 'ZZZ':
+                found_path = True
+                break
+    return steps
+
+def get_steps_part2(node_list, path, starter_node):
+    steps = 0
+    found_path = False
+    while not found_path:
+        for step in path:
+            if step == 'L':
+                starter_node = search_node(starter_node.left, node_list)
+            else:
+                starter_node = search_node(starter_node.right, node_list)
+            steps += 1
+            if starter_node.name[-1] == 'Z':
+                found_path = True
+                break
+    return steps
+
 if __name__ == '__main__':
     lines = read_lines('input.txt')
     path = lines[0].strip()
     nodes = lines[2:]
     node_list = list(map(parse_node, nodes))
-    steps = 0
-    actual_node = search_node('AAA', node_list)
-    found_path = False
-    while not found_path:
-        for step in path:
-            if step == 'L':
-                actual_node = search_node(actual_node.left, node_list)
-            else:
-                actual_node = search_node(actual_node.right, node_list)
-            steps += 1
-            if actual_node.name == 'ZZZ':
-                found_path = True
-                break
-    print(steps)
+    starter_node_part1 = search_node('AAA', node_list)
+    steps = get_steps(node_list, path, starter_node_part1)
+    print("Part 1: ", steps)
+    starter_nodes = list(filter(lambda node: node.name[-1] == 'A', node_list))
+    steps_per_node = []
+    for starter_node in starter_nodes:
+        steps_per_node.append(get_steps_part2(node_list, path, starter_node))
+    lcm_part2 = 1
+    for i in steps_per_node:
+        lcm_part2 = lcm_part2*i//gcd(lcm_part2, i)
+    print("Part 2: ", lcm_part2)
